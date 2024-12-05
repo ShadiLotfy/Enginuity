77f7f90e3e8a0664fd81fb2e741bd83f07fb3f8d
Shadi <shadi.moustafa.lotfy@gmail.com>
2024-12-05 15:30:30 +0200

    Added Live Captuer from OBD II port and OBD II IO

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+# Please lmk with a mention that u made it to here @7ambok4a
 import pandas as pd
+
+filepath = r"E:\AAST\Graduation Proj I\Datasets\archive (2)\exp2_19drivers_1car_1route.csv"
+df = pd.read_csv(filepath)
+print(df.head())
+
